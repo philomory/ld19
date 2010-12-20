760df510d15d50e47bbd2ea62a15726370a090f3
philomory <philomory@localhost>
2010-12-19 16:07:53 -1000

    Fix crashing bug

--- app/HiddenCaveEntrance.rb
+++ app/HiddenCaveEntrance.rb
@@ -1,6 +1,6 @@
 module LD19
   class Prop::HiddenCaveEntrance < Prop::CaveEntrance
-    def build
+    def build(properties = {})
       super
       destroy! unless $window.basic_player.has_all_keys?
     end
